3d3541905a9c915764e3919500a999da7da9f082
aa-ag <aaron.aguerrevere@gmail.com>
2020-06-26 22:29:01 -0500

    querySelectorAll version, working

---
+++ README.md
@@ -0,0 +1,10 @@
+# About Time
+
+About Time is a productivity Chrome Extension / Web App that helps users allocate their time by calculating the estimated reading time for websites or manually inserted text.
+
+## Feature requests
+
+* Ignore metadata
+* Account for `<a>` tags
+* Create reading speed ranges
+* Create custom-made reading speed test
--- script.js
+++ script.js
@@ -1,16 +1,53 @@
 run(document.body)
 
-function run(element){
-    if (element.hasChildNodes()) {
-        element.childNodes.forEach(run)
-    } else if (element.nodeType === Text.TEXT_NODE) {
-        section = element.textContent
-        count = section.length
-        console.log(count)
-    }
-} 
+let words = run(document.querySelectorAll('p'))
+
+function run(words) {
+    let count = 0
+	for (let i = 0; i < words.length; i++) {
+        count += words[i].textContent.split(' ').length
+	}
+	return count
+}
+
+console.log(`Approx reading time: ${Math.ceil(words / 200)} minutes`)
 
 
+// bewlow does not grabbing first paragraph from Reddit's wikipedia & code from Google.com
+
+// run(document.body)
+
+// function run(element){
+//     if (element.hasChildNodes()) {
+//         element.childNodes.forEach(run)
+//     } else if (element.nodeType === Text.TEXT_NODE) {
+//         if (element.textContent.match(/^[a-zA-Z]/)) {
+//             section = element.textContent.split(" ")
+//             console.log(section)
+//         } 
+//     } 
+// } 
+
+
+
+// run(document.body)
+
+// function run(element){
+//     if (element.hasChildNodes()) {
+//         element.childNodes.forEach(run)
+//     } else if (element.nodeType === Text.TEXT_NODE) {
+//         if (element.textContent.match(/^[a-zA-Z]/)) {
+//             section = element.textContent.split(" ")
+//             console.log(section.length)
+//         }
+//     }
+// } 
+
+
+// ### test to see length of each section
+
+        // element.textContent = element.textContent.length
+        // console.log(element.textContent) 
 
 // ### IF P GRABBED, LENGHT IS NUMB OF P'S
 
@@ -24,27 +61,25 @@ function run(element){
 
 // THIS WORKS PERFECT, FOR STATIC/MANUAL SITE
 
-/* 
+// function run() {
+//     let input = document.getElementById("text").value.trim().replace(/\s+/g," ");
+//     total = input.split(" ").length;
+//     document.getElementById("count").innerHTML = `${total} words.`;
 
-function run() {
-    let input = document.getElementById("text").value.trim().replace(/\s+/g," ");
-    total = input.split(" ").length;
-    document.getElementById("count").innerHTML = `${total} words.`;
-
-    let minutes = total / 150;
-    if (minutes < 1) {
-        document.getElementById("time").innerHTML = `Approx. reading time: less 1 a minute!`;
-    } else {
-        document.getElementById("time").innerHTML = `Approx. reading time: ${Math.ceil(minutes)} minutes.`;
-    }
-}
+//     let minutes = total / 150;
+//     if (minutes < 1) {
+//         document.getElementById("time").innerHTML = `Approx. reading time: less 1 a minute!`;
+//     } else {
+//         document.getElementById("time").innerHTML = `Approx. reading time: ${Math.ceil(minutes)} minutes.`;
+//     }
+// }
 
 
 
 // .replace(/(\r\n|\n|\r)/gm," ") includes every possibility as depends on encoding (mac, linux, windows)
 
 
-
+/*
 ~ Long version: for loop:
 
 function run() {
@@ -77,18 +112,12 @@ function run() {
 
 */
 
-/* ###### Replace text client-side experiment ###### 
+// replaceText(document.body)
 
-replaceText(document.body)
-
-function replaceText(element) {
-    if (element.hasChildNodes()) {
-        element.childNodes.forEach(replaceText)
-    } else if (element.nodeType === Text.TEXT_NODE) {
-      element.textContent = element.textContent.replace(/reddit/gi, '------')
-    }
-} 
-
-
-
-*/ 
+// function replaceText(element) {
+//     if (element.hasChildNodes()) {
+//         element.childNodes.forEach(replaceText)
+//     } else if (element.nodeType === Text.TEXT_NODE) {
+//       element.textContent = element.textContent.replace(/reddit/gi, '------')
+//     }
+// } 
