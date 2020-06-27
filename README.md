9ef23d63b8e31c762ef0d4067b5b86d051966630
aa-ag <aaron.aguerrevere@gmail.com>
2020-06-26 22:54:58 -0500

    edited README

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 
 About Time is a productivity Chrome Extension / Web App that helps users allocate their time by calculating the estimated reading time for websites or manually inserted text.
 
+## Asumptions
+
+- average adult reads ~200 words per minute
+- It's better to round up than down when estimating how long
+
 ## Feature requests
 
 * Ignore metadata
--- script.js
+++ script.js
@@ -100,38 +100,21 @@ console.log(`Approx reading time: ${Math.ceil(words / 200)} minutes`)
 // .replace(/(\r\n|\n|\r)/gm," ") includes every possibility as depends on encoding (mac, linux, windows)
 
 
-/*
-~ Long version: for loop:
+// ~ Long version: for loop:
 
-function run() {
-    let text = document.getElementById("input").value.trim();
-    let total = 0;
-    for (let i = 0; i < text.length; i++){
-        let currentCharacter = text[i];
-        if (currentCharacter == " "){
-            total += 1;
-        }
-    };
-    total += 1
-    document.getElementById("output").innerHTML = `${total} words`
-}
+// function run() {
+//     let text = document.getElementById("input").value.trim();
+//     let total = 0;
+//     for (let i = 0; i < text.length; i++){
+//         let currentCharacter = text[i];
+//         if (currentCharacter == " "){
+//             total += 1;
+//         }
+//     };
+//     total += 1
+//     document.getElementById("output").innerHTML = `${total} words`
+// }
 
-~ Asumptions: 
-
-- average adult reads 150 words-per-minute
-- it's better to round up than down when estimating how long it'll take to read a text
-
-~ Known Bugs:
-
-- leading and trailing white spaces: .trim()
-- extra spaces in between words: .replace(/ +/g, ' ')
-- line breaks: /(\r\n|\n|\r)/gm, "" // not sure how to incorporate this yet
-- spaces inside words
-- special characters
-- non "readable" text (nav bar, menu items, ads, etc)
-- empy run
-
-*/
 
 // replaceText(document.body)
 
